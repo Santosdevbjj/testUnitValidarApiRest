95e6e59f4baa54e906d98f6c04b92a89dde266c5
Sergio <santossergiorealbjj@outlook.com>
2025-11-12 16:49:10 -0300

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO beer (id, name, brand, max, quantity) VALUES (1, 'Heineken', 'Heineken', 50, 10);
+INSERT INTO beer (id, name, brand, max, quantity) VALUES (2, 'Budweiser', 'ABInBev', 100, 20);
